441c85493cad108f6f6800ed5cb4fc75e7b4bb79
henriquecscode <henriquecostasousa.hcs@gmail.com>
2023-04-03 12:33:09 +0100

    Producer and Company exchange messages

--- src/main/java/Agents/Company/CompanyAgent.java
+++ src/main/java/Agents/Company/CompanyAgent.java
@@ -3,6 +3,15 @@ package Agents.Company;
 
 import Company.Company;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.ContractNetResponder;
 
 public class CompanyAgent extends Agent {
     private Company company;
@@ -10,9 +19,68 @@ public class CompanyAgent extends Agent {
     public CompanyAgent(Company company) {
         super();
         this.company = company;
+
+        // randomize two locations (ad hoc)
+        // determine the best path (deterministic and common with step 2)
+        // figure out which paths can offer
+        // give a price for those paths
     }
 
-    protected void setup() {
 
+    protected void setup() {
+        registerAgent();
+        addResponderBehaviour();
+    }
+
+    private void registerAgent() {
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("company");
+        sd.setName(getName());
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+    }
+
+    public void addResponderBehaviour(){
+        MessageTemplate template = MessageTemplate.and(
+                MessageTemplate.MatchProtocol("route-request"),
+                MessageTemplate.MatchPerformative(ACLMessage.CFP)
+        );
+        addBehaviour(
+                new RouteRequestContractNetResponder(
+                        this, MessageTemplate.MatchPerformative(ACLMessage.CFP))
+        );
+    }
+
+    class RouteRequestContractNetResponder extends ContractNetResponder {
+        public RouteRequestContractNetResponder(Agent a, MessageTemplate template) {
+            super(a, template);
+        }
+
+        protected ACLMessage handleCfp(ACLMessage cfp) {
+            ACLMessage reply = cfp.createReply();
+            reply.setPerformative(ACLMessage.PROPOSE);
+            reply.setContent("I will do it for free!!!" + " (from " + myAgent.getLocalName() +")" );
+            // ...
+            return reply;
+        }
+
+        protected void handleRejectProposal(ACLMessage cfp, ACLMessage propose, ACLMessage reject) {
+            System.out.println(myAgent.getLocalName() + " got a reject...");
+        }
+
+        protected ACLMessage handleAcceptProposal(ACLMessage cfp, ACLMessage propose, ACLMessage accept) {
+            System.out.println(myAgent.getLocalName() + " got an accept!");
+            ACLMessage result = accept.createReply();
+            result.setPerformative(ACLMessage.INFORM);
+            result.setContent("this is the result");
+
+            return result;
+        }
     }
 }
--- src/main/java/Agents/ProducerAgent.java
+++ src/main/java/Agents/ProducerAgent.java
@@ -1,20 +1,33 @@
 package Agents;
 
 
+import Company.Company;
+import Network.Location.House;
 import Producer.Producer;
 import Producer.Production;
+import jade.core.AID;
 import jade.core.Agent;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.proto.ContractNetInitiator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
 
 public class ProducerAgent extends Agent {
     private Producer producer;
+    private static int producerAgentId = 0;
+    private List<AID> companies = new ArrayList<>();
 
     public ProducerAgent(Producer producer) {
         super();
         this.producer = producer;
+        producerAgentId++;
     }
 
     protected void setup() {
@@ -31,6 +44,71 @@ public class ProducerAgent extends Agent {
             DFService.register(this, dfd);
         } catch (FIPAException fe) {
             fe.printStackTrace();
+        }
+    }
+
+    public void getCompanies() {
+        DFAgentDescription template = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("company");
+        template.addServices(sd);
+        DFAgentDescription[] result = null;
+        try {
+            result = DFService.search(this, template);
+
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+        for (DFAgentDescription description : result) {
+            companies.add(description.getName());
+        }
+    }
+
+    public void testProducerRequest() {
+        Random random = new Random();
+        List<House> houses = producer.network.getHouses();
+        House house = houses.get(random.nextInt(houses.size()));
+
+        if (companies.isEmpty()) {
+            getCompanies();
+        }
+
+        addBehaviour(new RouteRequestContractNetInit(this, new ACLMessage(ACLMessage.CFP)));
+
+    }
+
+    class RouteRequestContractNetInit extends ContractNetInitiator {
+        public RouteRequestContractNetInit(Agent a, ACLMessage cfp) {
+            super(a, cfp);
+        }
+
+        protected Vector prepareCfps(ACLMessage cfp) {
+
+            cfp.setContent("Hello");
+            cfp.setProtocol("route-request");
+            for(AID company : companies) {
+                cfp.addReceiver(company);
+            }
+            Vector v = new Vector();
+            v.add(cfp);
+            return v;
+        }
+
+        protected void handleAllResponses(Vector responses, Vector acceptances) {
+
+            System.out.println("got " + responses.size() + " responses!");
+
+            for(int i=0; i<responses.size(); i++) {
+                ACLMessage response = (ACLMessage) responses.get(i);
+                ACLMessage msg = ((ACLMessage) responses.get(i)).createReply();
+                System.out.println("Producer got an answer from " + response.getSender().getName() + " with content " + response.getContent());
+                msg.setPerformative(ACLMessage.ACCEPT_PROPOSAL); // OR NOT!
+                acceptances.add(msg);
+            }
+        }
+
+        protected void handleAllResultNotifications(Vector resultNotifications) {
+            System.out.println("got " + resultNotifications.size() + " result notifs!");
         }
     }
 }

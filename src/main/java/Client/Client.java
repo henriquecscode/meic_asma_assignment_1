f2a7421841e124890433e786fc5b8b26c5a07779
henriquecscode <henriquecostasousa.hcs@gmail.com>
2023-04-08 12:14:51 +0100

    additional getter

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -4,11 +4,15 @@ import Network.Location.House;
 import Network.Network;
 
 public class Client {
-    private final Network network;
+    public final Network network;
     private final House house;
 
     Client(Network network, House house) {
         this.network = network;
         this.house = house;
+    }
+
+    public House getHouse() {
+        return house;
     }
 }
